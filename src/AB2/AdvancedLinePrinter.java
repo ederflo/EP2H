7db1abea9f743b8616943c897b3bca79d32e6f9c
Florian Eder <f.eder@arduvi.com>
2025-03-21 19:09:34 +0100

    implemented AB2

---
+++ src/AB2/AdvancedLinePrinter.java
@@ -0,0 +1,146 @@
+package AB2;
+
+import AB2.Interfaces.Font;
+import AB2.Interfaces.LineBuffer;
+
+/**
+ * The AdvancedLinePrinter class is designed to render Braille text representations to standard output
+ * using an auto-scaling memory efficient line buffer and a customizable font.
+ *
+ * <p>This class provides methods for printing Braille characters and strings, both specified by corresponding
+ * ASCII characters. All print operations at character level are delegated to the line buffer.</p>
+ * <p>The line buffer can also be modified by inserting and deleting characters at specified positions.
+ * It can be sent to the system's standard output (console) at any time by calling the {@code flush()} method,
+ * which also clears the line buffer.</p>
+ */
+public class AdvancedLinePrinter {
+    /*
+        REMARK: Type of variable lineBuffer is {@code LineBuffer} (DO NOT CHANGE!).
+        Your BrailleLineBuffer is a LineBuffer, so it can be referenced by this variable.
+        Details on this behavior, so called interfacing, will be explained later in this semester.
+        You can use {@code LineBuffer} as if it would be of type {@code BrailleLineBuffer}
+        (provided that you do not add further public methods or variables to {@code BrailleLineBuffer}).
+    */
+    private final LineBuffer lineBuffer;  // buffer that holds output (bitmaps of printed braille characters)
+
+    private final Font font;        // font used to render output
+
+    // TODO: your variables go here
+    private int spacing;
+
+    /**
+     * Constructs an AdvancedLinePrinter for processing printable Braille text.
+     *
+     * @param font       the font that defines the printable representation for characters.
+     *                   <p>Precondition: ( font != null )</p>
+     * @param spacing    the number of blank screen spaces (ASCII columns) between Braille characters (bitmaps).
+     *                   <p>Precondition: ( spacing >= 0) </p>
+     * @param lineBuffer a lineBuffer used by the AdvancedLinePrinter
+     *                   <p>Precondition: ( lineBuffer != null )</p>
+     */
+    public AdvancedLinePrinter(Font font, int spacing, LineBuffer lineBuffer) {
+        this.lineBuffer = lineBuffer;
+        this.font = font;
+        this.spacing = spacing;
+    }
+
+    /**
+     * Method clears the line buffer.
+     */
+    public void clearLine() {
+        lineBuffer.clearBuffer();
+    }
+
+    /**
+     * Method appends a single Braille character to the line buffer.
+     *
+     * <p>The method retrieves a printable Braille representation (bitmap) of the given ASCII character
+     * from {@code font} and appends it to the line buffer.</p>
+     *
+     * @param character an ASCII character to be converted to a printable Braille representation
+     *                  and printed into the line buffer.
+     */
+    public void printCharacter(char character) {
+        lineBuffer.push(font.getBitmap(character));
+    }
+
+    /**
+     * Removes the last Braille character (bitmap) from the line buffer.
+     *
+     * <p> If the buffer is empty, no action is performed. This operation is useful for
+     * simulating a backspace functionality in a Braille text editing process.</p>
+     *
+     * <p>Postconditions:<BR>
+     * 1. If the buffer contains Braille characters, the last character is removed.<BR>
+     * 2. If the buffer is empty, no changes occur.</p>
+     */
+    public void printBackSpace() {
+        lineBuffer.pop();
+    }
+
+    /**
+     * Method appends a string to the line buffer by appending each of the string's characters to the line buffer.
+     *
+     * @param string the string to be printed.
+     */
+    public void printString(String string) {
+        for (char c : string.toCharArray())
+            printCharacter(c);
+    }
+
+    /**
+     * Method flushes the content of the line buffer by printing each row to the standard output (console),
+     * and then clearing the buffer.
+     */
+    public void flush() {
+        for (String line : lineBuffer.renderScanlines(getSpacing()))
+            System.out.println(line);
+        clearLine();
+    }
+
+
+    /**
+     * Inserts a character into the line buffer at the specified position.
+     *
+     * <p>The method retrieves a Braille bitmap representation of the given ASCII character
+     * from {@code font} and inserts it into the line buffer at the provided position.
+     *
+     * @param position  the index in the line buffer where the character should be inserted.
+     *                  An invalid position must be handled by the buffer.
+     * @param character the ASCII character to be converted to a Braille bitmap and inserted.
+     */
+    public void insertCharacter(int position, char character) {
+        lineBuffer.insert(position, font.getBitmap(character));
+    }
+
+    /**
+     * Deletes a Braille character from the line buffer at the specified position.
+     * The characters following the deleted one are shifted to the left to fill the gap.
+     *
+     * @param position the index of the character to be deleted.
+     *                 An invalid position must be handled by the buffer.
+     */
+    public void deleteCharacter(int position) {
+        lineBuffer.delete(position);
+    }
+
+    /**
+     * Sets the spacing between Braille characters.
+     *
+     * @param spacing the number of blank screen spaces (ASCII columns) between Braille characters.
+     *                Must be greater than or equal to 0.
+     */
+    public void setSpacing(int spacing) {
+        this.spacing = spacing;
+    }
+
+    /**
+     * Retrieves the current spacing between Braille characters.
+     *
+     * @return the number of blank screen spaces (ASCII columns) currently used
+     * as spacing between Braille characters.
+     */
+    public int getSpacing() {
+        return spacing;
+    }
+}
